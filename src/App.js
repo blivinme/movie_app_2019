a29ff26dd8e9cbd6fc48fc7a598121b222826bcc
movingbro <54231339+blivinme@users.noreply.github.com>
2020-11-04 23:54:01 +0900

    11/4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-function Food(){
-  return <h1>I like Potato</h1>;
+function Food({ favourite }){
+  return <h1>I like {favourite}</h1>;
 }
 
 function App() {
   return (
   <div>
     <h1>Hello!!!!</h1>
-    <Food
-      fav="kimchi"
-      something={true}
-      papapapa{["hello", 1, 2,3,4,true]} 
-      />
+    <Food favourite="kimchi" />
+    <Food favourite="ramen" />
+    <Food favourite="samgiopsal" />
+    <Food favourite="chukumi" />
   </div>
   );
  }      
