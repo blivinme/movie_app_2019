4439439e04e581e9dfa75a72120d3456fcaeb66c
movingbro <54231339+blivinme@users.noreply.github.com>
2020-11-07 23:50:37 +0900

    11/07

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React from 'react';
+const foodILike = [
+  {
+    id: 1,
+    name: "Kinchi",
+    image: "https://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image: "https://images.chosun.com/resizer/LqiOo-kFtKiAUawSeNYkIGIisaQ=/464x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/IZOXUQBP4QVT4ONUPAIDA6347M.jpg",
+    rating: 4.7
+  }
+]
 
 function Food({ name, picture }){
   return <div>
-    
-  </div>I like {name}</h1>;
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name} />
+  </div>
 }
-
-const foodILike = [
-  {
-    name: "Kinchi",
-    image: "ㅇㄹㅇㄹㅇㄹㅇㄹ"
-  },
-  {
-    name: "Samgyeopsal",
-    image: "ㅇㄹㅇㄹㅇㄹㅇㄹ"
-  }
-]
 
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
+    {foodILike.map(dish => (
+      <Food key={dish.id} name={dish.name} picture={dish.image} />
+    ))}
     </div>
-  );
+    );
  }      
 
 export default App;
