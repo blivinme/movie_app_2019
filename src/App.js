2baa24e2d56d06bedcc3cabb3e6a6341802342ae
movingbro <54231339+blivinme@users.noreply.github.com>
2020-11-08 23:52:54 +0900

    11/08

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
-const foodILike = [
-  {
-    id: 1,
-    name: "Kinchi",
-    image: "https://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image: "https://images.chosun.com/resizer/LqiOo-kFtKiAUawSeNYkIGIisaQ=/464x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/IZOXUQBP4QVT4ONUPAIDA6347M.jpg",
-    rating: 4.7
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movie: []
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000)
   }
-]
+  render(){
+    const { isLoading } = this.state;
+  return <div>{isLoading ? "Loading..." : "We are ready"}</div>
 
-function Food({ name, picture }){
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} alt={name} />
-  </div>
-}
 
-function App() {
-  return (
-    <div>
-    {foodILike.map(dish => (
-      <Food key={dish.id} name={dish.name} picture={dish.image} />
-    ))}
-    </div>
-    );
- }      
+  }
+}   
 
 export default App;
