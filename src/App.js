702039dbb892acaa3928ebbacddd6bc1389d6019
movingbro <54231339+blivinme@users.noreply.github.com>
2020-11-14 18:35:06 +0900

    11/14

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movie: []
   };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  }
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000)
+    this.getMovies();
   }
   render(){
     const { isLoading } = this.state;
   return <div>{isLoading ? "Loading..." : "We are ready"}</div>
-
-
   }
 }   
 
